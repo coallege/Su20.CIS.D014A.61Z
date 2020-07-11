b8ec997b29ee2824b5a78381c69392d244e2ac2e
coalpha <22552895+coalpha@users.noreply.github.com>
2020-07-11 12:33:39 -0700

    Commit before work
    The stuff in this commit has been sitting in here for a while.
    I just didn't actually get to commit it until now so here.

---
+++ InClassDemos/DemoWeek2_Tuesday/FrmDemoWeek2_Tuesday.Designer.vb
@@ -0,0 +1,153 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FrmDemoWeek2_Tuesday
+  Inherits System.Windows.Forms.Form
+
+  'Form overrides dispose to clean up the component list.
+  <System.Diagnostics.DebuggerNonUserCode()> _
+  Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+    Try
+      If disposing AndAlso components IsNot Nothing Then
+        components.Dispose()
+      End If
+    Finally
+      MyBase.Dispose(disposing)
+    End Try
+  End Sub
+
+  'Required by the Windows Form Designer
+  Private components As System.ComponentModel.IContainer
+
+  'NOTE: The following procedure is required by the Windows Form Designer
+  'It can be modified using the Windows Form Designer.  
+  'Do not modify it using the code editor.
+  <System.Diagnostics.DebuggerStepThrough()> _
+  Private Sub InitializeComponent()
+        Me.LblMiles = New System.Windows.Forms.Label()
+        Me.TxtMiles = New System.Windows.Forms.TextBox()
+        Me.TxtYards = New System.Windows.Forms.TextBox()
+        Me.LblYards = New System.Windows.Forms.Label()
+        Me.TxtFeet = New System.Windows.Forms.TextBox()
+        Me.LblFeet = New System.Windows.Forms.Label()
+        Me.TxtInches = New System.Windows.Forms.TextBox()
+        Me.LblInches = New System.Windows.Forms.Label()
+        Me.BtnCalculate = New System.Windows.Forms.Button()
+        Me.LstOutput = New System.Windows.Forms.ListBox()
+        Me.SuspendLayout()
+        '
+        'LblMiles
+        '
+        Me.LblMiles.AutoSize = True
+        Me.LblMiles.Location = New System.Drawing.Point(32, 31)
+        Me.LblMiles.Name = "LblMiles"
+        Me.LblMiles.Size = New System.Drawing.Size(45, 20)
+        Me.LblMiles.TabIndex = 0
+        Me.LblMiles.Text = "&Miles"
+        '
+        'TxtMiles
+        '
+        Me.TxtMiles.Location = New System.Drawing.Point(132, 27)
+        Me.TxtMiles.Name = "TxtMiles"
+        Me.TxtMiles.Size = New System.Drawing.Size(146, 26)
+        Me.TxtMiles.TabIndex = 1
+        '
+        'TxtYards
+        '
+        Me.TxtYards.Location = New System.Drawing.Point(132, 72)
+        Me.TxtYards.Name = "TxtYards"
+        Me.TxtYards.Size = New System.Drawing.Size(146, 26)
+        Me.TxtYards.TabIndex = 3
+        '
+        'LblYards
+        '
+        Me.LblYards.AutoSize = True
+        Me.LblYards.Location = New System.Drawing.Point(32, 76)
+        Me.LblYards.Name = "LblYards"
+        Me.LblYards.Size = New System.Drawing.Size(51, 20)
+        Me.LblYards.TabIndex = 2
+        Me.LblYards.Text = "&Yards"
+        '
+        'TxtFeet
+        '
+        Me.TxtFeet.Location = New System.Drawing.Point(132, 114)
+        Me.TxtFeet.Name = "TxtFeet"
+        Me.TxtFeet.Size = New System.Drawing.Size(146, 26)
+        Me.TxtFeet.TabIndex = 5
+        '
+        'LblFeet
+        '
+        Me.LblFeet.AutoSize = True
+        Me.LblFeet.Location = New System.Drawing.Point(32, 118)
+        Me.LblFeet.Name = "LblFeet"
+        Me.LblFeet.Size = New System.Drawing.Size(42, 20)
+        Me.LblFeet.TabIndex = 4
+        Me.LblFeet.Text = "&Feet"
+        '
+        'TxtInches
+        '
+        Me.TxtInches.Location = New System.Drawing.Point(132, 157)
+        Me.TxtInches.Name = "TxtInches"
+        Me.TxtInches.Size = New System.Drawing.Size(146, 26)
+        Me.TxtInches.TabIndex = 7
+        '
+        'LblInches
+        '
+        Me.LblInches.AutoSize = True
+        Me.LblInches.Location = New System.Drawing.Point(32, 161)
+        Me.LblInches.Name = "LblInches"
+        Me.LblInches.Size = New System.Drawing.Size(57, 20)
+        Me.LblInches.TabIndex = 6
+        Me.LblInches.Text = "&Inches"
+        '
+        'BtnCalculate
+        '
+        Me.BtnCalculate.Location = New System.Drawing.Point(132, 214)
+        Me.BtnCalculate.Name = "BtnCalculate"
+        Me.BtnCalculate.Size = New System.Drawing.Size(145, 47)
+        Me.BtnCalculate.TabIndex = 8
+        Me.BtnCalculate.Text = "Calculate"
+        Me.BtnCalculate.UseVisualStyleBackColor = True
+        '
+        'LstOutput
+        '
+        Me.LstOutput.FormattingEnabled = True
+        Me.LstOutput.ItemHeight = 20
+        Me.LstOutput.Location = New System.Drawing.Point(37, 280)
+        Me.LstOutput.Name = "LstOutput"
+        Me.LstOutput.Size = New System.Drawing.Size(241, 84)
+        Me.LstOutput.TabIndex = 9
+        '
+        'FrmDemoWeek2_Tuesday
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(332, 398)
+        Me.Controls.Add(Me.LstOutput)
+        Me.Controls.Add(Me.BtnCalculate)
+        Me.Controls.Add(Me.TxtInches)
+        Me.Controls.Add(Me.LblInches)
+        Me.Controls.Add(Me.TxtFeet)
+        Me.Controls.Add(Me.LblFeet)
+        Me.Controls.Add(Me.TxtYards)
+        Me.Controls.Add(Me.LblYards)
+        Me.Controls.Add(Me.TxtMiles)
+        Me.Controls.Add(Me.LblMiles)
+        Me.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.Name = "FrmDemoWeek2_Tuesday"
+        Me.Text = "Project1 Lenth Converions"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents LblMiles As Label
+    Friend WithEvents TxtMiles As TextBox
+    Friend WithEvents TxtYards As TextBox
+    Friend WithEvents LblYards As Label
+    Friend WithEvents TxtFeet As TextBox
+    Friend WithEvents LblFeet As Label
+    Friend WithEvents TxtInches As TextBox
+    Friend WithEvents LblInches As Label
+    Friend WithEvents BtnCalculate As Button
+    Friend WithEvents LstOutput As ListBox
+End Class
---
+++ InClassDemos/DemoWeek2_Tuesday/FrmDemoWeek2_Tuesday.vb
@@ -0,0 +1,48 @@
+﻿Public Class FrmDemoWeek2_Tuesday
+   '*****************************************************
+   '*** Program: DemoWeek2_Tuesday - sample start of proj1.
+   '*** Author : Art Linn
+   '*** Date   : 7/7/2020
+   '*****************************************************
+
+   Private Sub FrmDemoWeek2_Tuesday_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+      'Setup program if needed
+   End Sub
+
+   Private Sub BtnCalculate_Click(sender As Object, e As EventArgs) Handles BtnCalculate.Click
+      'Calculates the metric lengths from english lengths
+      On Error Resume Next
+      Dim intMiles As Integer       'Input from user form
+      Dim intYards As Integer       'Input from user form
+      Dim intFeet As Integer        'Input from user form
+      Dim intInches As Integer      'Input from user form
+      Dim intTotalInches As Integer 'calculated from input
+      Dim DblTotalMeters As Double  'Calculated using TotalInches
+      Dim IntKilometers As Integer  'Calculated Klicks
+      Dim dblMeters As Double       'Calculated Meters
+      Dim DblCentimeters As Double  'Calculated Centimeters
+
+      'Input the data
+      intMiles = TxtMiles.Text
+      intYards = TxtYards.Text
+      intFeet = TxtFeet.Text
+      intInches = TxtInches.Text
+
+      'Calculate the lengths in metric values
+      intTotalInches = (63360 * intMiles) +
+                       (36 * intYards) +
+                       (12 * intFeet) +
+                       intInches
+      DblTotalMeters = intTotalInches / 39.37
+      IntKilometers = Int(DblTotalMeters / 1000)
+      dblMeters = DblTotalMeters - (IntKilometers * 1000)
+      'Calculate the Meters and Centimeters next
+
+
+      'Output the results
+      LstOutput.Items.Add("The metric length is:")
+      LstOutput.Items.Add(IntKilometers & " kilometers")
+      LstOutput.Items.Add(dblMeters & " meters")
+
+   End Sub
+End Class
