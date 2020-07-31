6b8996ae7c01c92e0da575dda2b65aeb19f0224f
coalpha <22552895+coalpha@users.noreply.github.com>
2020-07-31 15:29:54 -0700

    FPVB
    Yin and Yang.
    Pull and Push.
    Having a Job and Functional Programming.

--- Homeworks/Ex7_2/Ex7_2_9_13/frmEx7_2_9_13.vb
+++ Homeworks/Ex7_2/Ex7_2_9_13/frmEx7_2_9_13.vb
@@ -1,18 +1,17 @@
 ﻿Public Class frmEx7_2_9_13
   Private Sub btnDisplay9_Click() Handles btnDisplay9.Click
-    'The first four lines of Numbers.txt contain the numbers 2, 6, 7, and 8.
-    'Dim numbers() As String = IO.File.ReadAllLines("Numbers.txt")
     Dim numbers() = {"2", "6"}
-    Dim query = From num In numbers
-                Select CInt(num)
+    Dim query = numbers.Select(AddressOf Int)
     lstOutput.Items.Add(query(0) + query(1))
   End Sub
+
+  Function Identity(Of T)(val As T) As T
+    Return val
+  End Function
+
   Private Sub btnDisplay13_Click() Handles btnDisplay13.Click
     Dim nums() As Integer = {12, 5, 7, 10, 3, 15, 4}
-    Dim query = From num In nums
-                Where num > 10
-                Order By num Descending
-                Select num
+    Dim query = nums.Where(Function(num) num > 10).OrderByDescending(AddressOf Identity)
     lstOutput.DataSource = query.ToList
     lstOutput.SelectedItem = Nothing
   End Sub
